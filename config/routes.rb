3b9073cf0c9ffc5b0cf28531960736a2f1e3af30
Hasan Ahmed <ahmed.hasan09@gmail.com>
2015-12-04 16:21:36 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
 
   resources :users
 
-  #get 'users/new'
-
   root 'static_pages#home'
   
   get 'help' => 'static_pages#help'
@@ -13,5 +11,9 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
 
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,6 +6,9 @@ def setup
 	@user = User.new(name: "Example User", email: "user@example.com",
 									password: "foobar", password_confirmation: "foobar")
 end
+test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
+  end 
 
   test "should be valid" do
   	assert @user.valid?
