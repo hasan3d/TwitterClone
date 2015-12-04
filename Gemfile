cfe2521f2ab5b5a1b09e0cb926e1afc0c675a5fb
Hasan Ahmed <ahmed.hasan09@gmail.com>
2015-12-04 23:48:57 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,12 +5,16 @@ gem 'rails', '4.2.4'
 gem 'bcrypt'
 gem 'bootstrap-sass', '~> 3.3.5'
 gem 'sass-rails', '~> 5.0'
+gem 'will_paginate'          
+gem 'bootstrap-will_paginate', '0.0.10'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'faker'
+
 
 
 group :development, :test do
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,6 +14,10 @@ module SessionsHelper
 	    cookies.permanent.signed[:user_id] = user.id
 	    cookies.permanent[:remember_token] = user.remember_token	
 	end
+# Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
 
 	# Forget a persistent session
 	def forget(user)
@@ -44,5 +48,14 @@ module SessionsHelper
 		session.delete(:user_id)
 		@current_user = nil
 	end
+# Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
