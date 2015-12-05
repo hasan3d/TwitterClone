31461a168e2994077cedb7ece1bc6fab39e580f5
Hasan Ahmed <ahmed.hasan09@gmail.com>
2015-12-05 13:45:56 +0000

    Add account activations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,9 +6,9 @@ def setup
 	@user = User.new(name: "Example User", email: "user@example.com",
 									password: "foobar", password_confirmation: "foobar")
 end
-test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
-  end 
+	test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?(:remember, '')
+  end	 
 
   test "should be valid" do
   	assert @user.valid?
@@ -54,4 +54,5 @@ end
 		assert_not @user.valid?	
 
 	end
+	
 end
